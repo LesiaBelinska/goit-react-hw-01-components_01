e72189383c65541e1a50db1ca47be3f3fe910309
LesiaBelinska <lm.kudak@gmail.com>
2021-10-06 15:33:54 +0300

    add Profile component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
 import './App.css';
+import { Profile } from './components/Profile/Profile'
+import user from './data/user.json'
 
 function App() {
   return (
     <div className="App">
+      <Profile
+  name={user.name}
+  tag={user.tag}
+  location={user.location}
+  avatar={user.avatar}
+  stats={user.stats}
+/>
        
     </div>
   );
